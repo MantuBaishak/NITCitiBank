a210be00a2d6b4af4e9ca48d90a0536fbcbd7ded
TL <TL@gmail.com>
2020-04-12 02:16:50 +0530

    ghkjl

--- DemoProj1/src/com/nt/beans/Utility.java
+++ DemoProj1/src/com/nt/beans/Utility.java
@@ -12,5 +12,7 @@ public class Utility {
 		System.out.println("Utility m1() method :1234");
         System.out.println("abcd");
 	}
-
+   public void m2() {
+	   System.out.println("Utility.m2() method");
+   }
 }
